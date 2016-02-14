5209c29cd7742e58a9a0364bdde1c9ebb378d286
Kevin Comas <nodist@nodist.io>
2016-02-14 01:17:05 -0500

    auto make dist

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+mkdir dist
+mkdir ./dist/lib
+
 npm install
 
 cd node_modules/node-forge
 npm install
 npm run bundle
+cd ../..
+cp ./node_modules/node-forge/js/forge.bundle.js ./dist/lib
+cp ./node_modules/fernet/fernetBrowser.js ./dist/lib
+tsc --module commonjs
